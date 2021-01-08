b2ccc9fb4300826a38c2727ff16ccb72f6222f52
Rasmus Frøstrup <rasm364p@edu.mercantec.dk>
2021-01-08 09:52:06 +0100

    hello

---
+++ App3/App3/App3/About_Me.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class About_Me : ContentPage
+    {
+        public About_Me()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App3/App3/App3/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3
+{
+    public partial class App : Application
+    {
+        public NavigationPage About_Me { get; set; }
+        public NavigationPage Skills { get; set; }
+        public NavigationPage Education { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+            About_Me = new NavigationPage(new About_Me());
+            Skills = new NavigationPage(new Skills());
+            Education = new NavigationPage(new Education());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
